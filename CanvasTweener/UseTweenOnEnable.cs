690f5f8a5e79744d6407ce7b5e325ac5620a4e85
Khizer Khalid <khizer.khalid@tintash.com>
2018-09-13 04:01:43 +0500

    v1.0

---
+++ CanvasTweener/UseTweenOnEnable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseTweenOnEnable : MonoBehaviour
+{
+	void OnEnable ()
+	{
+		GetComponent<UITweener> ().ResetToBeginning ();
+		GetComponent<UITweener> ().enabled = true;		
+	}
+}
